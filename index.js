f303d75336a5a1bcb1a86ae7c4e12953be9687cd
gblvenkatesh <79314164+gblvenkatesh@users.noreply.github.com>
2022-11-02 11:03:15 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let entries = count + " - "
+    saveEl.textContent += entries
+    count = 0
+    countEl.textContent = 0
+}
+
+
+
+
+
+
+
+
+
+
